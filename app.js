a62db1375810aa749a70ffd6357de50e42122c1b
Tarun <tarunpahade6969@gmail.com>
2023-03-20 17:28:33 +0530

    r

--- app.js
+++ app.js
@@ -644,7 +644,7 @@ client.messages
   app.post("/webhook", (req, res) => {
     // Parse the request body from the POST
     let body = req.body;
-  console.log(body);
+  
     // Check the Incoming webhook message
     console.log(JSON.stringify(req.body, null, 2));
   
@@ -657,18 +657,17 @@ client.messages
         req.body.entry[0].changes[0].value.messages &&
         req.body.entry[0].changes[0].value.messages[0]
       ) {
-     console.log('now reply');
         let phone_number_id =
           req.body.entry[0].changes[0].value.metadata.phone_number_id;
         let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
         let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-       console.log('hola');
-        axios.post(   "https://graph.facebook.com/v12.0/" +
-        phone_number_id +
-        "/messages?access_token=" +process.env.TOKEN,{
-         // method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-          
-         
+      console.log(from,msg_body);
+        axios({
+          method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+          url:
+            "https://graph.facebook.com/v16.0/" +
+            phone_number_id +
+            "/messages?access_token=" +process.env.TOKEN,
           data: {
             messaging_product: "whatsapp",
             to: from,
